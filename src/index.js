f993b02eb14ef34b65920a6ec53eb0b79006ff69
Daniel Schefer <danielschefer2007@hotmail.com>
2025-07-04 21:17:11 -0300

    0.2

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-/*const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-} */
+import { Personagem } from "./models/personagem.js";
 
-const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
-
-const personagemAna = {
-    nome: 'Ana',
-    vida: 8,
-    mana: 10,
-    level: 5,
-    tipo: 'Guerreira'
-}
-
-const personagemPedrinho = new Personagem()
+const personagemPedrinho = new Personagem();
 personagemPedrinho.nome = "Pedrinho";
 personagemPedrinho.vida = 7;
 personagemPedrinho.mana = 12;
 personagemPedrinho.level = 5;
 personagemPedrinho.tipo = "Mago";
-personagemPedrinho.descricao
+
+const personagemJose = new Personagem();
+
+console.log(personagemPedrinho);
--- src/models/personagem.js
+++ src/models/personagem.js
@@ -1,8 +1,15 @@
-class Personagem {
+export class Personagem {
     nome
     vida
     mana
     level
     tipo
     descricao
+
+    obterInsignia() {
+        if (this.level >= 5) {
+            return `Implacavel ${this.tipo}`;
+        } 
+        return `${this.tipo} iniciante`;
+    }
 }
